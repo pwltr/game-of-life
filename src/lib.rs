b73f9d3499678d06300d58f1f1279d32b82f60a2
Philipp Walter <philippwalter@pm.me>
2022-02-02 20:59:31 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,258 @@
+mod utils;
+
+use fixedbitset::FixedBitSet;
+use wasm_bindgen::prelude::*;
+
+extern crate fixedbitset;
+extern crate js_sys;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: FixedBitSet,
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn new(width: u32, height: u32) -> Universe {
+        utils::set_panic_hook();
+
+        let size = (width * height) as usize;
+        let cells = FixedBitSet::with_capacity(size);
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
+    }
+
+    // Set the width of the universe.
+    // Resets all cells to the dead state.
+    // pub fn set_width(&mut self, width: u32) {
+    //     self.width = width;
+    //     // self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+
+    //     let mut next = self.cells.clone();
+
+    //     for row in 0..self.height {
+    //         for col in 0..width {
+    //             let idx = self.get_index(row, col);
+    //             next.set(idx, false);
+    //         }
+    //     }
+
+    //     self.cells = next;
+    // }
+
+    // Set the height of the universe.
+    // Resets all cells to the dead state.
+    // pub fn set_height(&mut self, height: u32) {
+    //     self.height = height;
+    //     // self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
+
+    //     let mut next = self.cells.clone();
+
+    //     for row in 0..height {
+    //         for col in 0..self.width {
+    //             let idx = self.get_index(row, col);
+    //             next.set(idx, false);
+    //         }
+    //     }
+
+    //     self.cells = next;
+    // }
+
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                // log!(
+                //     "cell[{}, {}] has {} live neighbors and is {:?}",
+                //     row,
+                //     col,
+                //     live_neighbors,
+                //     cell,
+                // );
+
+                let next_state = match (cell, live_neighbors) {
+                    (true, x) if x < 2 => false,
+                    (true, 2) | (true, 3) => true,
+                    (true, x) if x > 3 => false,
+                    (false, 3) => true,
+                    (otherwise, _) => otherwise,
+                };
+
+                next.set(idx, next_state);
+
+                // log!("    => will be `{:?}`", next_state);
+            }
+        }
+
+        // log!("current cells: `{:?}`", self.cells);
+        // log!("next cells: `{:?}`", next);
+
+        self.cells = next;
+    }
+
+    pub fn toggle_cell(&mut self, row: u32, column: u32) {
+        let idx = self.get_index(row, column);
+        self.cells.set(idx, !self.cells[idx]);
+    }
+
+    pub fn add_glider(&mut self, row: u32, column: u32) {
+        let pattern = &[
+            (row, column),
+            ((row + 2) % self.height, column),
+            ((row + 1) % self.height, (column + 1) % self.width),
+            ((row + 1) % self.height, (column + 2) % self.width),
+            ((row + 2) % self.height, (column + 1) % self.width),
+        ];
+
+        for (row, col) in pattern.iter().cloned() {
+            let idx = (row * self.width + col) as usize;
+            self.cells.set(idx, true);
+        }
+    }
+
+    pub fn add_pulsar(&mut self, row: u32, column: u32) {
+        let pattern = &[
+            ((row) % self.height, (column + 2) % self.width),
+            ((row) % self.height, (column + 3) % self.width),
+            ((row) % self.height, (column + 4) % self.width),
+            ((row) % self.height, (column + 8) % self.width),
+            ((row) % self.height, (column + 9) % self.width),
+            ((row) % self.height, (column + 10) % self.width),
+            ((row + 2) % self.height, (column) % self.width),
+            ((row + 2) % self.height, (column + 5) % self.width),
+            ((row + 2) % self.height, (column + 7) % self.width),
+            ((row + 2) % self.height, (column + 12) % self.width),
+            ((row + 3) % self.height, (column) % self.width),
+            ((row + 3) % self.height, (column + 5) % self.width),
+            ((row + 3) % self.height, (column + 7) % self.width),
+            ((row + 3) % self.height, (column + 12) % self.width),
+            ((row + 4) % self.height, (column) % self.width),
+            ((row + 4) % self.height, (column + 5) % self.width),
+            ((row + 4) % self.height, (column + 7) % self.width),
+            ((row + 4) % self.height, (column + 12) % self.width),
+            ((row + 5) % self.height, (column + 2) % self.width),
+            ((row + 5) % self.height, (column + 3) % self.width),
+            ((row + 5) % self.height, (column + 4) % self.width),
+            ((row + 5) % self.height, (column + 8) % self.width),
+            ((row + 5) % self.height, (column + 9) % self.width),
+            ((row + 5) % self.height, (column + 10) % self.width),
+            ((row + 7) % self.height, (column + 2) % self.width),
+            ((row + 7) % self.height, (column + 3) % self.width),
+            ((row + 7) % self.height, (column + 4) % self.width),
+            ((row + 7) % self.height, (column + 8) % self.width),
+            ((row + 7) % self.height, (column + 9) % self.width),
+            ((row + 7) % self.height, (column + 10) % self.width),
+            ((row + 8) % self.height, (column) % self.width),
+            ((row + 8) % self.height, (column + 5) % self.width),
+            ((row + 8) % self.height, (column + 7) % self.width),
+            ((row + 8) % self.height, (column + 12) % self.width),
+            ((row + 9) % self.height, (column) % self.width),
+            ((row + 9) % self.height, (column + 5) % self.width),
+            ((row + 9) % self.height, (column + 7) % self.width),
+            ((row + 9) % self.height, (column + 12) % self.width),
+            ((row + 10) % self.height, (column) % self.width),
+            ((row + 10) % self.height, (column + 5) % self.width),
+            ((row + 10) % self.height, (column + 7) % self.width),
+            ((row + 10) % self.height, (column + 12) % self.width),
+            ((row + 12) % self.height, (column + 2) % self.width),
+            ((row + 12) % self.height, (column + 3) % self.width),
+            ((row + 12) % self.height, (column + 4) % self.width),
+            ((row + 12) % self.height, (column + 8) % self.width),
+            ((row + 12) % self.height, (column + 9) % self.width),
+            ((row + 12) % self.height, (column + 10) % self.width),
+        ];
+
+        for (row, col) in pattern.iter().cloned() {
+            let idx = (row * self.width + col) as usize;
+            self.cells.set(idx, true);
+        }
+    }
+
+    pub fn set_random(&mut self) {
+        let size = (self.width * self.height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+
+        for i in 0..size {
+            cells.set(i, js_sys::Math::random() < 0.4);
+        }
+
+        self.cells = cells;
+    }
+
+    pub fn clear(&mut self) {
+        let size = (self.width * self.height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+
+        for i in 0..size {
+            cells.set(i, false);
+        }
+
+        self.cells = cells;
+    }
+}
+
+impl Universe {
+    // Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &FixedBitSet {
+        &self.cells
+    }
+
+    // Set cells to be alive in a universe by passing the row and column
+    // of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells.set(idx, true);
+        }
+    }
+}
